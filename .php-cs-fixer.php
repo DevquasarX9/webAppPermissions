69aeddc830518b270a9f015516f94b59392ac4da
Ivelin Ivanov <9060426+ivosgem@users.noreply.github.com>
2025-10-16 13:25:15 +0300

    Init commit.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,136 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$rules = [
+    '@PSR1' => true,
+    '@PSR2' => true,
+    '@PSR12' => true,
+    '@PhpCsFixer' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'binary_operator_spaces' => [
+        'default' => 'single_space',
+        'operators' => ['=>' => null],
+    ],
+    'blank_line_before_statement' => [
+        'statements' => [
+            'default',
+            'for',
+            'foreach',
+            'if',
+            'case',
+            'return',
+            'switch',
+            'throw',
+            'try',
+        ],
+    ],
+    'cast_spaces' => [
+        'space' => 'none',
+    ],
+    'class_attributes_separation' => [
+        'elements' => [
+            'case' => 'one',
+            'const' => 'none',
+            'method' => 'one',
+            'property' => 'none',
+            'trait_import' => 'none',
+        ],
+    ],
+    'class_definition' => true,
+    'concat_space' => [
+        'spacing' => 'one',
+    ],
+    'explicit_string_variable' => false,
+    'fully_qualified_strict_types' => true,
+    'heredoc_to_nowdoc' => true,
+    'include' => true,
+    'increment_style' => ['style' => 'post'],
+    'linebreak_after_opening_tag' => true,
+    'constant_case' => true,
+    'magic_method_casing' => true,
+    'magic_constant_casing' => true,
+    'method_chaining_indentation' => true,
+    'native_function_casing' => true,
+    'no_alias_functions' => true,
+    'no_extra_blank_lines' => [
+        'tokens' => [
+            'extra',
+            'throw',
+            'use',
+        ],
+    ],
+    'no_blank_lines_after_phpdoc' => true,
+    'no_empty_phpdoc' => true,
+    'no_empty_statement' => true,
+    'no_leading_namespace_whitespace' => true,
+    'no_mixed_echo_print' => ['use' => 'echo'],
+    'no_multiline_whitespace_around_double_arrow' => true,
+    'multiline_whitespace_before_semicolons' => [
+        'strategy' => 'no_multi_line',
+    ],
+    'no_short_bool_cast' => true,
+    'no_singleline_whitespace_before_semicolons' => true,
+    'no_spaces_around_offset' => true,
+    'no_unneeded_control_parentheses' => true,
+    'no_unreachable_default_argument_value' => true,
+    'no_useless_return' => true,
+    'no_unused_imports' => true,
+    'no_whitespace_before_comma_in_array' => true,
+    'normalize_index_brace' => true,
+    'not_operator_with_successor_space' => false,
+    'object_operator_without_whitespace' => true,
+    'ordered_imports' => ['sort_algorithm' => 'alpha'],
+    'php_unit_internal_class' => false,
+    'php_unit_method_casing' => ['case' => 'snake_case'],
+    'php_unit_test_class_requires_covers' => false,
+    'phpdoc_indent' => true,
+    'general_phpdoc_tag_rename' => true,
+    'phpdoc_inline_tag_normalizer' => true,
+    'phpdoc_tag_type' => true,
+    'phpdoc_no_access' => true,
+    'phpdoc_no_package' => true,
+    'phpdoc_no_useless_inheritdoc' => true,
+    'phpdoc_scalar' => true,
+    'phpdoc_single_line_var_spacing' => true,
+    'phpdoc_summary' => true,
+    'phpdoc_to_comment' => true,
+    'phpdoc_trim' => true,
+    'phpdoc_types' => true,
+    'phpdoc_var_without_name' => true,
+    'psr_autoloading' => true,
+    'self_accessor' => true,
+    'simplified_null_return' => false,
+    'single_import_per_statement' => true,
+    'single_line_comment_spacing' => true,
+    'single_line_comment_style' => [
+        'comment_types' => ['hash'],
+    ],
+    'single_quote' => true,
+    'space_after_semicolon' => true,
+    'standardize_not_equals' => true,
+    'trailing_comma_in_multiline' => true,
+    'trim_array_spaces' => true,
+    'unary_operator_spaces' => true,
+    'whitespace_after_comma_in_array' => true,
+    'yoda_style' => [
+        'equal' => false,
+        'identical' => false,
+        'less_and_greater' => false,
+    ],
+];
+
+$config = new Config();
+
+$finder = Finder::create()
+    ->in(__DIR__)
+    ->name('*.php')
+    ->notPath('githooks')
+    ->notPath('vendor');
+
+return $config->setFinder($finder)
+    ->setRules($rules)
+    ->setRiskyAllowed(true)
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setUsingCache(true);
